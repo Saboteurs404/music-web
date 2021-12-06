939583c08812b018270fa0c8ec78de315e531dcc
juice999 <461712835@qq.com>
2021-12-06 16:24:14 +0800

    [更新]歌单、用户、评论、评价part完成

--- music-server/src/main/java/com/choi/music/MusicApplication.java
+++ music-server/src/main/java/com/choi/music/MusicApplication.java
@@ -2,9 +2,11 @@ package com.choi.music;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@MapperScan("com.choi.music.dao")
+
 @SpringBootApplication
+@MapperScan("com.choi.music.dao")
 public class MusicApplication {
 
     public static void main(String[] args) {
